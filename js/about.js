973cb4656355b51a224f2a451f8d24ffbf3a0920
boygenius2 <ivang85601@gmail.com>
2025-10-24 14:28:08 -0700

    Tweet date sorting

--- js/about.js
+++ js/about.js
@@ -12,6 +12,18 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
+
+	const options = {
+  		day: "numeric",
+		year: "numeric",
+		month: "long"
+	};
+
+	// Map each date in tweet_array to its numerical value, then sort in ascending/descending order and remap these values into Date objects
+	const first_date = tweet_array.map(tweet => tweet.time.valueOf()).sort((a, b) => {return a-b;}).map(time => new Date(time))[0].toLocaleDateString("en-US", options);
+	const last_date = tweet_array.map(tweet => tweet.time.valueOf()).sort((a, b) => {return b-a;}).map(time => new Date(time))[0].toLocaleDateString("en-US", options);
+	document.getElementById('firstDate').innerText = first_date;
+	document.getElementById('lastDate').innerText = last_date;
 }
 
 //Wait for the DOM to load
